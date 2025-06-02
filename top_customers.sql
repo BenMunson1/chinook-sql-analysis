7568cd7d3841e8203306a3d01cb8019e4151284f
BenMunson1 <100311728+BenMunson1@users.noreply.github.com>
2025-06-02 16:42:53 -0500

    Update top_customers.sql

--- top_customers.sql
+++ top_customers.sql
@@ -1,2 +1,11 @@
 -- Question: Who are the top 5 customers by total spending?
--- (SQL query will go here)
+SELECT 
+    c.FirstName || ' ' || c.LastName AS CustomerName,
+    ROUND(SUM(i.Total), 2) AS TotalSpent
+FROM Customer c
+JOIN Invoice i ON c.CustomerId = i.CustomerId
+GROUP BY CustomerName
+ORDER BY TotalSpent DESC
+LIMIT 5;
+-- Insight:
+-- Helena Holý is the top spender with over $49 in purchases.
