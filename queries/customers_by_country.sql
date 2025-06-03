5ad8546f3df5778eaf57ede8ca1e703a936c4273
BenMunson1 <100311728+BenMunson1@users.noreply.github.com>
2025-06-03 16:34:18 -0500

    Update customers_by_country.sql

--- queries/customers_by_country.sql
+++ queries/customers_by_country.sql
@@ -1,9 +1,14 @@
 -- Question: Which countries have the most customers?
 
-SELECT Country, COUNT(*) AS CustomerCount
-FROM Customer
-GROUP BY Country
-ORDER BY CustomerCount DESC;
+SELECT 
+  Country, 
+  COUNT(*) AS CustomerCount
+FROM 
+  Customer
+GROUP BY 
+  Country
+ORDER BY 
+  CustomerCount DESC;
 
 -- Insight:
 -- Most customers are from the USA, followed by Canada, and then France and Brazil.
