6073d5d37a9772cc86c28626ed424ebce95cac70
BenMunson1 <100311728+BenMunson1@users.noreply.github.com>
2025-06-03 16:34:00 -0500

    Update customers_by_city.sql

--- queries/customers_by_city.sql
+++ queries/customers_by_city.sql
@@ -1,9 +1,14 @@
 -- Question: Which city has the most customers, and how many per city?
 
-SELECT city, COUNT(*) AS num_customers
-FROM customer
-GROUP BY city
-ORDER BY num_customers DESC;
+SELECT 
+  city, 
+  COUNT(*) AS num_customers
+FROM 
+  customer
+GROUP BY 
+  city
+ORDER BY 
+  num_customers DESC;
 
 -- Insight:
 -- 6 cities have 2 customers each: São Paulo, Prague, Paris, Mountain View, London, and Berlin. The rest of the cities have 1 customer each.
