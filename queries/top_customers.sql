12a87431aa1e21d63954bf8b0e32346d6a00aa97
BenMunson1 <100311728+BenMunson1@users.noreply.github.com>
2025-06-03 16:34:39 -0500

    Update top_customers.sql

--- queries/top_customers.sql
+++ queries/top_customers.sql
@@ -3,10 +3,14 @@
 SELECT 
     c.FirstName || ' ' || c.LastName AS CustomerName,
     ROUND(SUM(i.Total), 2) AS TotalSpent
-FROM Customer c
-JOIN Invoice i ON c.CustomerId = i.CustomerId
-GROUP BY CustomerName
-ORDER BY TotalSpent DESC
+FROM 
+    Customer c
+JOIN 
+    Invoice i ON c.CustomerId = i.CustomerId
+GROUP BY 
+    CustomerName
+ORDER BY 
+    TotalSpent DESC
 LIMIT 5;
 
 -- Insight:
